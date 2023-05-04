26c2a8a83ea9607e1d9c702ba5ab52bb7a8d41fd
zklemily <ezhang1024@gmail.com>
2023-05-04 17:53:00 -0400

    main and network

--- main.java
+++ main.java
@@ -15,7 +15,7 @@ public class main {
     private int numVertices;    //all vertices in the doc
     private int actualVertices; //actual vertices without junk edges
     private double tau;
-    Network graph = new Network();
+    private Network graph = new Network();
 
     public int loadGraphFromDataSet(String filePath, double tau) {
         this.tau = tau;
@@ -208,6 +208,7 @@ public class main {
     }
 
     private int getUserByUsername(String username) {
+
         return 0;
     }
 
